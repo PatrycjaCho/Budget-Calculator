d9b7d4bcc7a158890f7fe816adadeee8ade01fb9
PatrycjaCho <130142478+PatrycjaCho@users.noreply.github.com>
2023-05-09 14:50:39 +0100

    Form update
    Form will save in LS and display upon refreshing. Add button works as intended upon click

--- app.js
+++ app.js
@@ -1,47 +1,75 @@
 // all variables
-const submitBtn = document.getElementById("submitBtn")
-const openModal = document.getElementById("openModal")
-const formModal = document.querySelector('.formModal')
+const submitBtn = document.getElementById("submitBtn");
+const cancelBtn = document.getElementById("cancelBtn");
+const openModal = document.getElementById("openModal");
+const formModal = document.querySelector('.formModal');
 
-const balanceInput = document.getElementById("balance")
-const moneyTotal = document.getElementById("moneyTotal")
+let moneyTotal = document.getElementById("moneyTotal");
+let balance;
+
 
 // make it check if there is already data in storage 
+let budgetData = JSON.parse(localStorage.getItem("budgetData"));
+
+//make it display data from storage with new function UpdateUI
+if (budgetData) {
+  updateUI(budgetData);
+}
+else {
+  moneyTotal.textContent = "£0.00";
+}
+document.querySelector("form").reset();
+
+// the updateUI function 
+function updateUI(budgetData) {
+  moneyTotal.textContent = `£${budgetData.balance}`;
+  document.getElementById("balance").value = budgetData.balance;
+  document.getElementById("save").value = budgetData.save;
+  document.getElementById("rent").value = budgetData.rent;
+  document.getElementById("groceries").value = budgetData.groceries;
+  document.getElementById("other").value = budgetData.other;
+}
 
 // open form on clicking
 openModal.addEventListener("click", () => {
-  formModal.showModal()
+  formModal.showModal();
 })
 
 submitBtn.addEventListener("click", () => {
-  formModal.close()
+  formModal.close();
 })
 
 
 // save form input data to LS
-submitBtn.addEventListener("click", function () {
+submitBtn.addEventListener("click", function (event) {
+  event.preventDefault();
 
-  const balance = document.getElementById("balance").value;
-  const spend = document.getElementById("spend").value;
-  const rent = document.getElementById("rent").value;
-  const groceries = document.getElementById("groceries").value;
-  const other = document.getElementById("other").value;
+  // get the values of inputs
+  balance = parseFloat(document.getElementById("balance").value);
+  const save = parseFloat(document.getElementById("save").value);
+  const rent = parseFloat(document.getElementById("rent").value);
+  const groceries = parseFloat(document.getElementById("groceries").value);
+  const other = parseFloat(document.getElementById("other").value);
 
-  moneyTotal.textContent = `£${balanceInput.value}`;
-  balanceInput.addEventListener("change", () => {
-    const balance = balanceInput.value
-    moneyTotal.textContent = `£${balance}`
-  })
-
+  // put the values in LS
   const budgetData = {
     balance: balance,
-    spend: spend,
+    save: save,
     rent: rent,
     groceries: groceries,
     other: other
   };
-
   localStorage.setItem("budgetData", JSON.stringify(budgetData));
+
+  // update moneyTotal
+  moneyTotal.textContent = `£${balance}`;
+
+  // clear form 
+  document.querySelector("form").reset();
+});
+
+// make cancel button also clear form
+cancelBtn.addEventListener("click", () => {
+  document.querySelector("form").reset();
 })
 
-// update moneyTotal
